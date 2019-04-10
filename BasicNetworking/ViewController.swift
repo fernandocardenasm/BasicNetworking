f96b4313ea780a741cb581dc1256893c6b068a88
Fernando Cardenas <fernando.cardenasm@gmail.com>
2019-04-10 13:48:51 +0200

    Initial Commit

---
+++ BasicNetworking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicNetworking
+//
+//  Created by Fernando Cardenas on 10.04.19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
