55ea00b31387f0e7fad68b899176d25a20d283c3
Fernando Cardenas <fernando.cardenasm@gmail.com>
2019-04-11 16:08:43 +0200

    Add Mock for URLSessionProtocol

--- BasicNetworking/DatabaseManager.swift
+++ BasicNetworking/DatabaseManager.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 class DatabaseManager {
-    private let session: NetworkSessionService
+    private let session: URLSessionProtocol
 
-    init(session: NetworkSessionService = URLSession.shared) {
+    init(session: URLSessionProtocol = URLSession.shared) {
         self.session = session
     }
 
     func loadPlaces(from url: URL, completionHandler: @escaping ((Result<Data, Error>) -> Void)) {
-        session.loadPlaces(from: url) { data, _, error in
+        let task = session.dataTask(with: url) { (data, response, error) in
             if let error = error {
                 completionHandler(.failure(error))
             }
@@ -23,18 +23,44 @@ class DatabaseManager {
                 completionHandler(.success(data))
             }
         }
-    }
-}
-
-protocol NetworkSessionService {
-    func loadPlaces(from url: URL, completionHandler: @escaping ((Data?, URLResponse?, Error?) -> Void))
-}
-
-extension URLSession: NetworkSessionService {
-    func loadPlaces(from url: URL, completionHandler: @escaping ((Data?, URLResponse?, Error?) -> Void)) {
-        let task = dataTask(with: url) { data, response, error in
-            completionHandler(data, response, error)
-        }
         task.resume()
     }
 }
+
+//protocol NetworkSessionService {
+//    func loadPlaces(with url: URL, completionHandler: @escaping ((Data?, URLResponse?, Error?) -> Void))
+//}
+
+protocol URLSessionProtocol {
+    typealias DataTaskResult = (Data?, URLResponse?, Error?) -> Void
+
+    func dataTask(with request: URLRequest, completionHandler: @escaping DataTaskResult) -> URLSessionDataTask
+
+    func dataTask(with url: URL, completionHandler: @escaping DataTaskResult) -> URLSessionDataTask
+}
+extension URLSession: URLSessionProtocol {
+//    func dataTask(with request: URLRequest, completionHandler: @escaping DataTaskResult) -> URLSessionDataTaskProtocol {
+//        return (dataTask(with: request,
+//                         completionHandler: completionHandler) as URLSessionDataTask) as URLSessionDataTaskProtocol
+//    }
+//
+//    func dataTask(with url: URL, completionHandler: @escaping DataTaskResult) -> URLSessionDataTaskProtocol {
+//        return (dataTask(with: url,
+//                         completionHandler: completionHandler) as URLSessionDataTask) as URLSessionDataTaskProtocol
+//    }
+}
+
+//protocol URLSessionDataTaskProtocol {
+//    func resume()
+//}
+//
+//extension URLSessionDataTask: URLSessionDataTaskProtocol { }
+
+//extension URLSession: NetworkSessionService {
+//    func loadPlaces(from url: URL, completionHandler: @escaping ((Data?, URLResponse?, Error?) -> Void)) {
+//        let task = dataTask(with: url) { data, response, error in
+//            completionHandler(data, response, error)
+//        }
+//        task.resume()
+//    }
+//}
--- BasicNetworkingTests/DatabaseManagerTests.swift
+++ BasicNetworkingTests/DatabaseManagerTests.swift
@@ -10,15 +10,17 @@ import XCTest
 
 class DatabaseManagerTests: XCTestCase {
     func test_loadPlaces() {
-        let session = NetworkSessionServiceMock()
+        let dataTask = URLSessionDataTaskMock()
+        let session = URLSessionMock(newDataTask: dataTask)
         let dataBaseManager = DatabaseManager(session: session)
+        let url = URL(fileURLWithPath: "url")
 
         session.data = nil
         session.response = nil
         session.error = NSError(domain: "", code: 0, userInfo: nil)
 
         let failureExp = expectation(description: "a failure should be returned")
-        dataBaseManager.loadPlaces(from: URL(fileURLWithPath: "url")) { result in
+        dataBaseManager.loadPlaces(from: url) { result in
             switch result {
             case .success:
                 XCTFail("It should not succeed")
@@ -27,10 +29,14 @@ class DatabaseManagerTests: XCTestCase {
             }
         }
         wait(for: [failureExp], timeout: 0.1)
+
+        XCTAssertEqual(session.url, url)
+        XCTAssertTrue(dataTask.resumeWasCalled)
     }
 
     func test_loadPlaces_notNil() {
-        let session = NetworkSessionServiceMock()
+        let dataTask = URLSessionDataTaskMock()
+        let session = URLSessionMock(newDataTask: dataTask)
         let dataBaseManager = DatabaseManager(session: session)
         let url = URL(fileURLWithPath: "url")
 
@@ -39,7 +45,7 @@ class DatabaseManagerTests: XCTestCase {
         session.error = nil
 
         let successExp = expectation(description: "a success should be returned")
-        dataBaseManager.loadPlaces(from: URL(fileURLWithPath: "url")) { result in
+        dataBaseManager.loadPlaces(from: url) { result in
             switch result {
             case .success:
                 successExp.fulfill()
@@ -48,15 +54,54 @@ class DatabaseManagerTests: XCTestCase {
             }
         }
         wait(for: [successExp], timeout: 0.1)
+
+        XCTAssertEqual(session.url, url)
+        XCTAssertTrue(dataTask.resumeWasCalled)
     }
 }
 
-class NetworkSessionServiceMock: NetworkSessionService {
+// class NetworkSessionServiceMock: NetworkSessionService {
+//    var data: Data?
+//    var response: URLResponse?
+//    var error: Error?
+//
+//    func loadPlaces(from url: URL, completionHandler: @escaping ((Data?, URLResponse?, Error?) -> Void)) {
+//        completionHandler(data, response, error)
+//    }
+// }
+
+class URLSessionMock: URLSessionProtocol {
+    var request: URLRequest?
+    var url: URL?
+
     var data: Data?
     var response: URLResponse?
     var error: Error?
 
-    func loadPlaces(from url: URL, completionHandler: @escaping ((Data?, URLResponse?, Error?) -> Void)) {
+    let newDataTask: URLSessionDataTaskMock
+
+    init(newDataTask: URLSessionDataTaskMock) {
+        self.newDataTask = newDataTask
+    }
+
+    func dataTask(with request: URLRequest, completionHandler: @escaping DataTaskResult) -> URLSessionDataTask {
+        self.request = request
         completionHandler(data, response, error)
+        return newDataTask
+    }
+
+    func dataTask(with url: URL, completionHandler: @escaping DataTaskResult) -> URLSessionDataTask {
+        self.url = url
+        completionHandler(data, response, error)
+        return newDataTask
+    }
+}
+
+class URLSessionDataTaskMock: URLSessionDataTask {
+    var resumeWasCalled = false
+
+    override func resume() {
+        resumeWasCalled = true
+        // If super.resume() is called then this exception is returned: "NSInvalidArgumentException", "*** -resume cannot be sent to abstract instance of class
     }
 }
