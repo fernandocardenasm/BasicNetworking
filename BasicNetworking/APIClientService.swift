82a1ad55a55764cbebe9e8049b9de3eb6438ae5e
Fernando Cardenas <fernando.cardenasm@gmail.com>
2019-04-16 22:24:14 +0200

    Create GlobalConstants

--- BasicNetworking/APIClientService.swift
+++ BasicNetworking/APIClientService.swift
@@ -14,22 +14,30 @@ protocol APIClientService {
 
 class MarvelAPIClientServiceImpl: APIClientService {
     let session: URLSessionProtocol
-    let endpoint = "https://gateway.marvel.com:443/v1/public/characters?apikey=bc950974b1ba1d3491bfd681ba43ed03"
 
     init(session: URLSessionProtocol = URLSession(configuration: .default)) {
         self.session = session
     }
 
     func send<T>(_ request: T, completion: Result<T.Response, Error>) where T: APIRequest {
-
+        let endpointURL = makeEndpointURL(for: request)
+        
+        guard let url = endpointURL else {
+            return
+        }
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+            } else if let data = data {
+            }
+        }
+        task.resume()
     }
 
-    func makeEndpoint<T: APIRequest>(for request: T) -> URLComponents {
+    func makeEndpointURL<T: APIRequest>(for request: T) -> URL? {
         var components = URLComponents()
         components.scheme = "https"
-        components.host = "host"
-        components.path = request.resourceName
-
-        return components
+        components.host = GlobalConstants.MarvelAPI.baseEndpoint
+        components.path = request.resourceName + "?apikey=\(GlobalConstants.MarvelAPI.publicKey)"
+        return components.url
     }
 }
--- BasicNetworking/GlobalConstants.swift
+++ BasicNetworking/GlobalConstants.swift
@@ -11,5 +11,6 @@ struct GlobalConstants {
     struct MarvelAPI {
         static let publicKey = "bc950974b1ba1d3491bfd681ba43ed03"
         static let privateKey = "c4210a8106f063ee0a5075c7428b0de54ea58658"
+        static let baseEndpoint = "gateway.marvel.com:443/v1/public/"
     }
 }
