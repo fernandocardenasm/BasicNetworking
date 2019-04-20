0219ae78132a978782ffb7553730e2c7b897f30a
Fernando Cardenas <fernando.cardenasm@gmail.com>
2019-04-20 12:02:59 +0200

    Add Unit Tests

--- BasicNetworkingTests/MarvelAPIClientServiceTests.swift
+++ BasicNetworkingTests/MarvelAPIClientServiceTests.swift
@@ -32,4 +32,52 @@ class MarvelAPIClientService: XCTestCase {
         expect(parameters.last?.name).to(equal(GlobalConstants.MarvelAPI.Parameters.apiKey))
         expect(parameters.last?.value).to(equal(GlobalConstants.MarvelAPI.publicKey))
     }
+    
+    func test_filterParameters() {
+        let dataTask = URLSessionDataTaskMock()
+        let session = URLSessionMock(newDataTask: dataTask)
+        let service = MarvelAPIClientServiceImpl(session: session)
+        let filterParameters = makeFilterParameters()
+        let request = RequestMock(resourceName: "/resourceName",
+                                  parameters: filterParameters)
+        let parameters = service.filterParameters(for: request)
+        expect(parameters.count).to(equal(filterParameters.count))
+        parameters.forEach {
+            expect(filterParameters.keys.contains($0.name)).to(beTrue())
+            expect("\(filterParameters[$0.name] ?? "")").to(equal($0.value))
+        }
+    }
+    
+    func test_makeEndpointURL_validURL() {
+        let dataTask = URLSessionDataTaskMock()
+        let session = URLSessionMock(newDataTask: dataTask)
+        let service = MarvelAPIClientServiceImpl(session: session)
+        let filterParameters = makeFilterParameters()
+        let request = RequestMock(resourceName: "/resourceName",
+                                  parameters: filterParameters)
+        let url = service.makeEndpointURL(for: request)
+        expect(url).notTo(beNil())
+    }
+    
+    func makeFilterParameters() -> [String: Any] {
+        return ["int": 0,
+                "double": 1.0,
+                "string":"",
+                "boolean": true]
+    }
+}
+
+class RequestMock: APIRequest {
+    typealias Response = String
+    
+    let resourceName: String
+    let parameters: [String: Any]
+    
+    init(resourceName: String, parameters: [String: Any]) {
+        self.resourceName = resourceName
+        self.parameters = parameters
+    }
+    
+    func encode(to encoder: Encoder) throws {
+    }
 }
